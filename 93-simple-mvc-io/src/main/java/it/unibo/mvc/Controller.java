d409f44ed30b937ed06077988f587bdf5e17ff9b
Sajmir <sajmir.buzi@studio.unibo.it>
2023-11-23 17:10:32 +0100

    controllor and GUI implementation

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,63 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
-/**
- * Application controller. Performs the I/O.
- */
 public class Controller {
 
+    private File currentFile;
+
+    // Costruttore che imposta il file di default su "output.txt" nella cartella home dell'utente
+    public Controller() {
+        String homeFolder = System.getProperty("user.home");
+        String fileSeparator = System.getProperty("file.separator");
+        this.currentFile = new File(homeFolder + fileSeparator + "output.txt");
+    }
+
+    // Metodo per impostare un nuovo file corrente
+    public void setCurrentFile(File file) {
+        this.currentFile = file;
+    }
+
+    // Metodo per ottenere il file corrente
+    public File getCurrentFile() {
+        return currentFile;
+    }
+
+    // Metodo per ottenere il percorso del file corrente come stringa
+    public String getCurrentFilePath() {
+        return currentFile.getPath();
+    }
+
+    // Metodo per salvare una stringa nel file corrente
+    public void save(final String text) throws IOException {
+        try (PrintStream out = new PrintStream(currentFile, StandardCharsets.UTF_8)) {
+            out.println(text);
+        }
+    
+    }
+
+    public static void main(String[] args) {
+        // Esempio di utilizzo della classe Controller
+        Controller controller = new Controller();
+
+        // Impostare un nuovo file corrente (es. "nuovoFile.txt")
+        File nuovoFile = new File(System.getProperty("user.home") + System.getProperty("file.separator") + "nuovoFile.txt");
+        controller.setCurrentFile(nuovoFile);
+
+        // Ottenere e stampare il percorso del file corrente
+        System.out.println("Percorso del file corrente: " + controller.getCurrentFilePath());
+
+        try {
+            // Salvare una stringa nel file corrente
+            controller.save("Contenuto di esempio");
+            System.out.println("Contenuto salvato con successo.");
+        } catch (IOException e) {
+            System.err.println("Si è verificato un errore durante il salvataggio del contenuto.");
+            e.printStackTrace();
+        }
+    }
 }
+
--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,20 @@
 package it.unibo.mvc;
 
+import java.awt.LayoutManager;
+
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +22,64 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private final JFrame frame = new JFrame("My First Java GUI");
+
+    private SimpleGUI(final Controller controller){
+
+        // creazione oggetti e dichiarazione 
+        final JTextArea text = new JTextArea();
+        final LayoutManager layout = new BorderLayout();
+        final JPanel panel1 = new JPanel();
+        panel1.setLayout(layout);
+
+        //gestione buttone save
+        final JButton save = new JButton("save");
+
+        save.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    controller.save(text.getText());
+                }catch(IOException e1){
+                    JOptionPane.showMessageDialog(null, e1.getMessage(), "An error occurred", JOptionPane.ERROR_MESSAGE);
+
+                }  
+            }
+            
+        });
+
+        panel1.add(text,BorderLayout.CENTER);
+        panel1.add(save,BorderLayout.SOUTH);
+
+        frame.setContentPane(panel1);
+
+        frame.setDefaultCloseOperation(0);
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / 2, sh / 2);
+        frame.setLocationByPlatform(true);
+    }
+
+    private void display() {
+        frame.setVisible(true);
+    }
+
+    /**
+     * Launches the application.
+     *
+     * @param a
+     *            unused
+     */
+    public static void main(final String... a) {
+        final SimpleGUI gui = new SimpleGUI(new Controller());
+        gui.display();
+    }
+
+
+
+
+
 
 }
