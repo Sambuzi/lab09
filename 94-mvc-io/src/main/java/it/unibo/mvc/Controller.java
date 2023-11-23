7ef7bdf3db198c14378532dba5f499cbea69997e
Sajmir <sajmir.buzi@studio.unibo.it>
2023-11-23 17:12:03 +0100

    implementation of simpleController and SimpleGUI

--- 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,17 @@
 package it.unibo.mvc;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  */
 public interface Controller {
+    void setNextString(String nextString);
+
+    String getNextString();
+
+    List<String> getHistory();
+
+    void getCurrentString();
 
 }
--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,48 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    private String nextString;
+    private List<String> history = new ArrayList<>();
+
+
+    @Override
+    public void setNextString(String nextString) {
+        if(nextString==null){
+            throw new IllegalArgumentException("The string is null");
+
+        }
+
+        this.nextString=nextString;    
+    }
+
+    @Override
+    public String getNextString() {
+        return nextString;
+        
+    }
+
+    @Override
+    public List<String> getHistory() {
+        return new ArrayList<>(history);
+    }
+
+    @Override
+    public void getCurrentString() {
+        if(nextString==null){
+            throw new IllegalArgumentException("String null");
+
+        }
+        System.out.println(nextString);
+        history.add(nextString); 
+        
+    }
 
 }
